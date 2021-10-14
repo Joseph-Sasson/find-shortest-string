78b9d84f3f40576913ccd3268982d41e2c8d5207
Joseph <josephsasson1@gmail.com>
2021-10-14 14:51:20 -0400

    done

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,12 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortestString = arr[0]
+  
+  arr.forEach(string => {
+    if (string.length < shortestString.length){
+      shortestString = string
+    }
+  })
+  return shortestString
 }
 
 if (require.main === module) {
@@ -18,9 +25,21 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion']);
+  }
+
+  const avgTime = (Date.now() - startTime) / 1000;
+  console.log(avgTime);
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+  // Create new array to store first index of original array as shortest 
+  // Loop through original array and add first index to new array if it is smaller than the next else add the next
+  // Also loop through new array to see if the next index is smaller, if so replace that index
+  // Return new array
 // And a written explanation of your solution
